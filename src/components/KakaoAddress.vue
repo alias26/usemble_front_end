f77af38b934ab4fce452a61dc02133d941277cd5
alias26 <khmjuhee979@gmail.com>
2024-06-12 16:37:22 +0900

    feat: Add 카카오맵 API

---
+++ src/components/KakaoAddress.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="mb-2">
+        <div class="from-group search">
+            <label class="form-label">주소 검색</label>
+            <div class="d-flex mb-2">
+                <input
+                    class="form-control address-text"
+                    type="text"
+                    id="sample5_address"
+                    placeholder="주소를 입력해주세요"
+                />
+                <input
+                    class="btn btn-dark ms-2"
+                    type="button"
+                    @click="sample5_execDaumPostcode"
+                    value="주소 찾기"
+                />
+            </div>
+        </div>
+        <KakaoMapComponent ref="kakaoMap" />
+    </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import KakaoMapComponent from "./KakaoMap.vue";
+
+const kakaoMap = ref(null);
+
+onMounted(() => {
+    if (!window.daum) {
+        loadScript();
+    }
+});
+
+function loadScript() {
+    const script = document.createElement("script");
+    script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+    script.onload = () => {};
+
+    document.head.appendChild(script);
+}
+
+function sample5_execDaumPostcode() {
+    new window.daum.Postcode({
+        oncomplete: function (data) {
+            var addr = data.address; // 최종 주소 변수
+
+            // 주소 정보를 해당 필드에 넣는다.
+            document.getElementById("sample5_address").value = addr;
+            kakaoMap.value.getAddress(addr);
+        },
+    }).open();
+}
+</script>
+
+<style scoped>
+.search {
+    text-align: left;
+    font-weight: bold;
+}
+
+.address-text {
+    width: 75%;
+}
+</style>
