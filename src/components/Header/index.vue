7ba69d85596fc2ef727b936dc55675603b4702fc
김명환 <myeonghwan57@gmail.com>
2024-06-13 17:01:10 +0900

    feat:Add 헤더

---
+++ src/components/Header/index.vue
@@ -0,0 +1,134 @@
+<template>
+    <!--
+    이미지 가운데 두기
+     <nav>
+        <div id="nav2">
+            <ul>
+                <li><a href="#">어셈블 둘러보기</a></li>
+                <li><a id="usemble" href="#">어셈블!</a></li>
+            </ul>
+        </div>
+        <img alt="Vue logo" src="../../assets/logo.png" />
+        <div id="login_join">
+            <a class="mx-4" href="#">로그인</a>
+            <span>|</span>
+            <a class="mx-4" href="#">회원가입</a>
+        </div>
+    </nav> -->
+    <nav>
+        <div id="leftmenu">
+            <a id="nav_text" class="mx-4" href="#">어셈블 둘러보기</a>
+            <a id="usemble" class="mx-4" href="#">어셈블!</a>
+        </div>
+        <img id="logoimg" alt="Vue logo" src="../../assets/logo.png" />
+        <div id="rightmenu">
+            <a id="nav_text" class="mx-4" href="#">로그인</a>
+            <span>|</span>
+            <a id="nav_text" class="mx-4" href="#">회원가입</a>
+        </div>
+    </nav>
+</template>
+
+<script setup></script>
+
+<style scoped>
+* {
+    padding: 0;
+    margin: 0;
+    height: 80px;
+}
+nav {
+    width: 70%;
+    height: 80px;
+    margin: 0px auto;
+    min-width: 700px;
+    text-align: center;
+}
+a {
+    text-decoration: none;
+    color: black;
+    font-size: 17px;
+    height: 80px;
+    font-weight: 900;
+}
+#logoimg {
+    width: 80px;
+}
+#rightmenu {
+    height: 80px;
+    display: flex;
+    float: right;
+    margin-top: 30px;
+}
+#leftmenu {
+    height: 80px;
+    display: flex;
+    float: left;
+    margin-top: 30px;
+}
+#usemble {
+    color: #00a3e0;
+}
+/*
+이미지 가운데 두기
+ * {
+    padding: 0;
+    margin: 0;
+}
+ul,
+ol {
+    list-style: none;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+    font-size: 17px;
+}
+#usemble {
+    color: #00a3e0;
+}
+
+img {
+    width: 145px;
+    height: 80px;
+    padding: 0 30px;
+    margin: auto;
+}
+
+nav {
+    display: flex;
+    width: 70%;
+    overflow: hidden;
+    height: 80px;
+    background-color: #ffffff;
+    margin: 0px auto;
+}
+
+#nav2 > a {
+    display: block;
+    float: left;
+    font-size: 30px;
+    font-weight: 900;
+    line-height: 80px;
+    padding: 0 30px;
+}
+
+#nav2 > ul {
+    float: right;
+}
+
+#nav2 > ul li {
+    float: left;
+    line-height: 80px;
+    margin-left: 30px;
+}
+#login_join {
+    float: right;
+    line-height: 80px;
+}
+
+#menu {
+    display: flex;
+} */
+</style>
---
+++ src/components/UserInfo/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="userinfo">
+        <div id="userimg">
+            <img src="../../assets/photo31.jpg" />
+        </div>
+        <div>
+            <div>
+                <div id="host">호스트</div>
+                <div id="id">myeonghwan57</div>
+                <div id="intro">
+                    <div>안녕하세요.</div>
+                    <div>성남 인근에서 작고 아담한 와인모임을 진행하고 있습니다.</div>
+                    <div>와인에 관심있으시면 참여해주세요.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup></script>
+<style scoped>
+img {
+    border-radius: 50%;
+    width: 60px;
+}
+#userinfo {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    display: flex;
+}
+#userimg {
+    margin: 0 15px;
+}
+#host {
+    font-size: 12px;
+    font-weight: 900;
+    color: grey;
+}
+#id {
+    font-weight: 900;
+}
+#intro {
+    margin-top: 10px;
+    font-size: 14px;
+    font-weight: 500;
+    color: grey;
+}
+</style>
