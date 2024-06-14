a7c7aa2dd35e8611978bdcdad20a84aad82ffd01
alias26 <khmjuhee979@gmail.com>
2024-06-14 16:26:37 +0900

    rename: Change file names

--- src/views/HomeView/SocialCardList.vue
+++ src/views/HomeView/SocialCardList.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script setup>
-import SocialCard from "@/components/SocialComponents/SocialCard.vue";
+import SocialCard from "@/components/Social/SocialCard.vue";
 </script>
 
 <style scoped>
