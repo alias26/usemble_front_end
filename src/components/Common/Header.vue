786bdf68fe04a434a6204ae55ef611749c25ae35
김명환 <myeonghwan57@gmail.com>
2024-06-17 14:31:40 +0900

    design : Header css 변경

--- src/components/Common/Header.vue
+++ src/components/Common/Header.vue
@@ -7,12 +7,12 @@
         <RouterLink to="/"><img id="logoimg" alt="Vue logo" src="@/assets/logo.png" /></RouterLink>
         <div id="rightmenu">
             <RouterLink to="/login" id="nav_text" class="mx-4">로그인</RouterLink>
-            <span>|</span>
+            <span id="nav_text">|</span>
             <RouterLink to="/join/joinMember" id="nav_text" class="mx-4" href="#"
                 >회원가입</RouterLink
             >
-            <RouterLink to="/admin">admin</RouterLink>
-            <RouterLink class="ms-4" to="/myPage">user</RouterLink>
+            <RouterLink id="nav_text" to="/admin">admin</RouterLink>
+            <RouterLink id="nav_text" class="ms-4" to="/myPage">user</RouterLink>
         </div>
     </nav>
 </template>
@@ -47,15 +47,17 @@ a {
     height: 80px;
     display: flex;
     float: right;
-    margin-top: 30px;
 }
 #leftmenu {
     height: 80px;
     display: flex;
     float: left;
-    margin-top: 30px;
 }
 #usemble {
     color: #00a3e0;
+    margin-top: 30px;
+}
+#nav_text {
+    margin-top: 30px;
 }
 </style>
