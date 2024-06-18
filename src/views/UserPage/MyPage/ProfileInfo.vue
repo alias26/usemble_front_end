73c9f84a2b33007e01b2b826ea0336ee1edaf87e
김명환 <myeonghwan57@gmail.com>
2024-06-18 11:27:58 +0900

    design: ProfileInfo div 구성 위치 수정

--- src/views/UserPage/MyPage/ProfileInfo.vue
+++ src/views/UserPage/MyPage/ProfileInfo.vue
@@ -1,35 +1,45 @@
 <template>
     <div class="d-flex">
         <div id="pro" class="text-start">
-            <div class="mb-2 fs-4 fw-bolder">프로필</div>
-            <img id="proimg" src="../../../assets/photo31.jpg" alt="" />
+            <div class="d-flex justify-content-between">
+                <div class="mb-2 fs-4 fw-bolder">프로필</div>
 
-            <div class="d-flex mb-2">
-                <div class="d-flex me-1">
-                    <div class="me-1 rounded-pill fw-bold" id="ct-btn">모임</div>
-                    <div class="me-1 rounded-pill fw-bold" id="ct-btn">운동</div>
-                    <div class="rounded-pill fw-bold" id="ct-btn">자기계발</div>
-                </div>
-                <div>
+                <div class="">
                     <button
-                        class="btn btn-outline-secondary text-black rounded-pill"
-                        style="width: 80px; height: 40px; font-size: 15px; border-color: lightgray"
-                    >
-                        <i class="bi bi-pencil-fill"></i>
-                        수정
-                    </button>
-                </div>
-            </div>
-
-            <div>
-                <div class="bg-light p-4 mb-3">자기소개가 없습니다.</div>
-                <div class="mt-3 mb-5">
-                    <button
-                        class="btn border p-2 fw-bold"
+                        class="btn border fw-bold"
                         style="font-size: 13px; width: 100px; height: 40px; border-color: lightgray"
                     >
                         프로필 수정
                     </button>
+                </div>
+            </div>
+
+            <div class="d-flex">
+                <img id="proimg" src="../../../assets/photo31.jpg" alt="" />
+
+                <div class="d-flex flex-column mb-2 ps-4" style="width: 100%">
+                    <div class="d-flex">
+                        <div class="d-flex me-1">
+                            <div class="me-1 rounded-pill fw-bold" id="ct-btn">모임</div>
+                            <div class="me-1 rounded-pill fw-bold" id="ct-btn">운동</div>
+                            <div class="rounded-pill fw-bold" id="ct-btn">자기계발</div>
+                        </div>
+                        <div>
+                            <button
+                                class="btn btn-outline-secondary text-black rounded-pill"
+                                style="
+                                    width: 80px;
+                                    height: 40px;
+                                    font-size: 15px;
+                                    border-color: lightgray;
+                                "
+                            >
+                                <i class="bi bi-pencil-fill"></i>
+                                수정
+                            </button>
+                        </div>
+                    </div>
+                    <div id="intro_box" class="bg-light p-4 mt-3">자기소개가 없습니다.</div>
                 </div>
             </div>
 
@@ -71,10 +81,14 @@
     color: #558ccf;
     font-size: 14px;
     padding: 10px 15px;
+    height: 40px;
 }
 #proimg {
     border-radius: 50%;
     width: 150px;
     margin: 10px 0;
+}
+#intro_box {
+    width: 100%;
 }
 </style>
