30966f9921d81f45402fbff8ead8dd82ced71b24
alias26 <khmjuhee979@gmail.com>
2024-07-08 14:00:51 +0900

    feat: Add 리뷰 유효성 검사

--- src/views/UserPage/MyPage/UpdateReviewModal.vue
+++ src/views/UserPage/MyPage/UpdateReviewModal.vue
@@ -8,6 +8,7 @@
                 placeholder="리뷰를 작성해주세요."
                 v-model="review.rcontent"
             ></textarea>
+            <span id="warning" v-if="warning" style="color: red">{{ warnMessage }}</span>
         </template>
         <template v-slot:footer>
             <button
@@ -27,11 +28,13 @@
 <script setup>
 import ModalTemplate from "@/components/ModalTemplate.vue";
 import reviewAPI from "@/apis/reviewAPI";
-import { inject } from "vue";
+import { inject, ref } from "vue";
 
 const emit = defineEmits(["close"]);
 const review = inject("review");
 const isReview = inject("isReview");
+const warning = ref(false);
+const warnMessage = ref("");
 
 async function deleteReview() {
     try {
@@ -46,10 +49,31 @@ async function deleteReview() {
 
 async function updateReview() {
     try {
-        await reviewAPI.updateReview(JSON.parse(JSON.stringify(review.value)));
-        emit("close");
+        if (validateReview()) {
+            await reviewAPI.updateReview(JSON.parse(JSON.stringify(review.value)));
+            emit("close");
+        }
     } catch (error) {
         console.log(error);
+    }
+}
+
+function validateReview() {
+    const hyperLink = /(https?:\/\/[^\s]+)|(www\.[^\s]+)/gi;
+    const reviewCopy = { ...review.value };
+
+    if (reviewCopy.rcontent.replace(/[^a-zA-Z\uAC00-\uD7A3]/g, "").length == 0) {
+        warning.value = true;
+        warnMessage.value = "한글 또는 영어를 이용해서 리뷰를 작성해주세요.";
+        return false;
+    } else if (hyperLink.test(reviewCopy.rcontent)) {
+        warning.value = true;
+        warnMessage.value = "하이퍼링크는 리뷰에 포함하실 수 없습니다.";
+        return false;
+    } else {
+        warning.value = false;
+        warnMessage.value = "";
+        return true;
     }
 }
 </script>
--- src/views/UserPage/MyPage/WriteReviewModal.vue
+++ src/views/UserPage/MyPage/WriteReviewModal.vue
@@ -8,12 +8,13 @@
                 placeholder="리뷰를 작성해주세요."
                 v-model="review.rcontent"
             ></textarea>
+            <span id="warning" v-if="warning" style="color: red">{{ warnMessage }}</span>
         </template>
         <template v-slot:footer>
             <button
                 type="button"
                 class="btn btn-secondary"
-                @click="emit('close')"
+                @click="closeModal"
                 data-bs-dismiss="modal"
             >
                 닫기
@@ -25,21 +26,50 @@
 
 <script setup>
 import ModalTemplate from "@/components/ModalTemplate.vue";
-import { inject } from "vue";
+import { inject, ref } from "vue";
 import reviewAPI from "@/apis/reviewAPI";
 
 const emit = defineEmits(["close", "reload"]);
 const review = inject("review");
 const isReview = inject("isReview");
+const warning = ref(false);
+const warnMessage = ref("");
 
 async function writeReview() {
     try {
-        await reviewAPI.writeReview(JSON.parse(JSON.stringify(review.value)));
-        isReview.value = !isReview.value;
-        emit("close");
+        if (validateReview()) {
+            await reviewAPI.writeReview(JSON.parse(JSON.stringify(review.value)));
+            isReview.value = !isReview.value;
+            emit("close");
+        }
     } catch (error) {
         console.log(error);
     }
+}
+
+function validateReview() {
+    const hyperLink = /(https?:\/\/[^\s]+)|(www\.[^\s]+)/gi;
+    const reviewCopy = { ...review.value };
+
+    if (reviewCopy.rcontent.replace(/[^a-zA-Z가-힣]/g, "").length == 0) {
+        warning.value = true;
+        warnMessage.value = "한글 또는 영어를 이용해서 리뷰를 작성해주세요.";
+        return false;
+    } else if (hyperLink.test(reviewCopy.rcontent)) {
+        warning.value = true;
+        warnMessage.value = "하이퍼링크는 리뷰에 포함하실 수 없습니다.";
+        return false;
+    } else {
+        warning.value = false;
+        warnMessage.value = "";
+        return true;
+    }
+}
+
+function closeModal() {
+    warning.value = false;
+    warnMessage.value = "";
+    emit("close");
 }
 </script>
 
